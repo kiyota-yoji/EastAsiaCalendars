a36bcc1ed885fe9e11886051578fe7c346ac4bcc
Yoji Kiyota <kiyota.yoji@gmail.com>
2015-04-27 05:51:23 +0900

    add packaging scripts
    setup.py:
      scripts using distutils.core
    eacal/__init__.py:
      (empty)

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup, Extension
+
+setup(name = 'eacal',
+      version = '1.0',
+      description = 'Python modules for accessing East Asia calendars (Sexagenary cycle, Solar term, etc.)',
+      license = 'LGPL',
+      packages = ['eacal']
+      )
