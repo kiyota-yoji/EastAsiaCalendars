0c63f8a3fd6a80911dfacdedf547c3ef823ce4cd
kiyota-yoji <kiyota@KIYOTA-no-MacBook-Pro.local>
2015-05-07 11:19:06 +0900

    Rename shortcut options of EACal.__init__()

--- eacal/__init__.py
+++ eacal/__init__.py
@@ -6,26 +6,24 @@ import pytz
 class EACal:
 
     def __init__(self, lang=Lang.EN, tz=pytz.utc,
-                 shortcut_zh_t=False, shortcut_zh_s=False,
-                 shortcut_ja=False, shortcut_ko=False,
-                 shortcut_vi=False):
+                 zh_t=False, zh_s=False,
+                 ja=False, ko=False,
+                 vi=False):
         self.lang = lang
         self.tz = tz
-        if shortcut_zh_t:
+        if zh_t:
             self.lang = Lang.ZH_HANT
             self.tz = pytz.timezone('Asia/Hong_Kong')
-        elif shortcut_zh_s:
+        elif zh_s:
             self.lang = Lang.ZH_HANS
             self.tz = pytz.timezone('Asia/Shanghai')
-        elif shortcut_ja:
+        elif ja:
             self.lang = Lang.JA
             self.tz = pytz.timezone('Asia/Tokyo')
-        elif shortcut_ko:
+        elif ko:
             self.lang = Lang.KO
             self.tz = pytz.timezone('Asia/Seoul')
-        elif shortcut_vi:
+        elif vi:
             self.lang = Lang.VI
             self.tz = pytz.timezone('Asia/Ho_Chi_Minh')
-
-        print "lang=%d tz=%s" % (self.lang, self.tz)
 
