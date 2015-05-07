d5529320ffec6a944436a03db45cd5cd8e41024a
kiyota-yoji <kiyota@KIYOTA-no-MacBook-Pro.local>
2015-05-07 12:04:05 +0900

    Implemented EACal.get_cycle_ymd()

--- eacal/__init__.py
+++ eacal/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from lang import Lang
+import cycle
+from lang import Lang, str_cycle
+
 import pytz
+from datetime import datetime
+
 
 class EACal:
 
@@ -26,4 +30,24 @@ class EACal:
         elif vi:
             self.lang = Lang.VI
             self.tz = pytz.timezone('Asia/Ho_Chi_Minh')
+
+    def get_cycle_ymd(self, dt, id=False):
+
+        # if "dt" is a tuple, it is interpreted as (year, month, day)
+        if isinstance(dt, tuple):
+            dt = datetime(dt[0], dt[1], dt[2])
+        
+        # if "dt" is a naive datetime, it is interpreted using 
+        # the specified timezone.
+        if dt.tzinfo is None:
+            dt = self.tz.localize(dt)
+
+        (y_id, m_id, d_id) = cycle.cycle_ymd(dt)
+
+        if id:
+            return (y_id, m_id, d_id)
+        else:
+            return (str_cycle(y_id, self.lang), 
+                    str_cycle(m_id, self.lang), 
+                    str_cycle(d_id, self.lang))
 
